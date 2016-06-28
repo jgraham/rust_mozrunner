28929b5b1dd72a98d2cef00e6cce4e314f07e899
James Graham <james@hoppipolla.co.uk>
2016-06-28 20:33:52 +0100

    Remove pointless tests

--- src/lib.rs
+++ src/lib.rs
@@ -7,17 +7,3 @@ extern crate winreg;
 pub mod runner;
 
 pub use runner::platform::firefox_default_path;
-
-#[cfg(test)]
-mod test {
-    use runner::Runner;
-
-    use std::path::Path;
-
-    #[test]
-    fn it_works() {
-        let path = Path::new("/home/jgraham/develop/gecko/obj-x86_64-unknown-linux-gnu/dist/bin/firefox/").unwrap();
-        let mut fx_runner = runner::FirefoxRunner::new(path, None).unwrap();
-        fx_runner.start();
-    }
-}
